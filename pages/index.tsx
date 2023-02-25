5f08bc73d7892d7af01e471773bd56065de5fe40
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-25 18:03:23 +0100

    added link and header to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
 	return (
@@ -12,7 +13,14 @@ const Home: NextPage = () => {
 
 			<Header />
 			<main>
-				<div></div>
+				<div className="flex justify-center my-4 ">
+					<Link
+						className="font-bold text-3xl text-purple-600 cursor-pointer transition ease-in-out duration-150 active:scale-95 hover:drop-shadow-xl hover:scale-105"
+						href="/nft/details"
+					>
+						Go to the detailspage
+					</Link>
+				</div>
 			</main>
 		</div>
 	);
